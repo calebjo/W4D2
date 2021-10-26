efd9bd0cb09fe51eaf54f4c02665b758232cecf4
Alex Dziuba <alexander.dziuba@gmail.com>
2021-10-26 14:21:50 -0400

    Working on move_piece

--- chess/board.rb
+++ chess/board.rb
@@ -1,21 +1,26 @@
 require_relative "./piece.rb"
+require "byebug"
 
 class Board
 
     def self.grid
         Array.new(8) { Array.new(8) }
     end
-
+    
+    
     def initialize
         @rows = Board.grid
-        Board.populate
+        # debugger
+        # Board.populate
+        self.populate
     end
-
-    def self.populate
+    
+    def populate
         range = [0, 1, 6, 7]
+        # debugger
         @rows.each_with_index do |row, i|
             if range.include?(i)
-                row.map! {|pos| pos == Piece.new }
+                row.map! {|pos| pos = Piece.new }
             end
         end
     end
@@ -37,11 +42,19 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
-        
+        this_piece = @rows[start_pos]
+        @rows[end_pos] = this_piece
+        @rows[start_pos] = nil
     end
 
 end
 
 b = Board.new
-pos = [0,0]
+# pos = [0,0]
+b.populate
+b.print
+puts "-----------------------------"
+a = [1,0]
+b = [3,0]
+b.move_piece(a,b)
 b.print
