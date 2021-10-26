130bf9bfa31da7cb0203a5546b92e7d154df86e1
Alex Dziuba <alexander.dziuba@gmail.com>
2021-10-26 13:39:59 -0400

    Finished Employee

--- employees/employee.rb
+++ employees/employee.rb
@@ -41,15 +41,3 @@ class Manager < Employee
 
 end
 
-ceo = Manager.new("Any","CEO", 40000)
-m = Manager.new("Alex", "Manager", 10000,ceo)
-e1 = Employee.new("Caleb", "Employee", 6000, m)
-e2 = Employee.new("Bob", "Employee", 3000, m)
-e3 = Employee.new("Dave", "Employee", 4000, m)
-
-# p m 
-# p e1
-p ceo.bonus(3)
-p m.bonus(2)
-p e1.bonus(3)
-# p ceo
